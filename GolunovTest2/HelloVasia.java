2e335391315d24a84645da91d5ce3bef07aa0bf0
Boris Golynov <o1neill@yandex.ru>
2021-01-12 20:00:45 +0300

    Andersrn Test 2

---
+++ GolunovTest2/HelloVasia.java
@@ -0,0 +1,24 @@
+package GolunovTest;
+
+import java.util.Scanner;
+
+public class HelloVasia {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public  static String Hello() {
+
+        System.out.println("Введите имя: ");
+        String a = null;
+        if (scan.hasNextLine()) {
+            a = scan.nextLine();
+            if (a.equals("Вячеслав")) {
+                System.out.println("Привет " + a);
+            } else {
+                System.out.println("Нет такого имени.");
+            }
+        }
+        return a;
+    }
+
+}
---
+++ GolunovTest2/Main.java
@@ -0,0 +1,10 @@
+package GolunovTest;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HelloVasia hV = new HelloVasia();
+        hV.Hello();
+    }
+}
